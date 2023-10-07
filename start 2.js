b686980ef57e815e63b85adf1a9080aa02f230ed
Mohd Abdul Mannan <abdulmannan@Mohds-MacBook-Air.local>
2023-10-07 10:42:23 +0530

    starting js

---
+++ start 2.js
@@ -0,0 +1,12 @@
+// starting js with chai aur javascript
+// Chai and code series 
+// sheryians coding school
+// Coding Blocks
+
+ //variables
+ const accountId = 100;
+ let accountEmail = "abcd@gmail.com"
+ var accountPassword = "12345"
+ city = "Hyderabad"
+console.log(accountId);
+console.table([accountId, accountEmail, accountPassword, city]);  
