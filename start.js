46d58d1a65e2a1823dbc61ff03b568cfdbc282a6
Mohd Abdul Mannan <ma24042000@gmail.com>
2023-10-17 12:21:30 +0530

    17 oct

--- start.js
+++ start.js
@@ -4,12 +4,12 @@
 // Coding Blocks
 
  //variables
- const accountId = 100;
- let accountEmail = "abcd@gmail.com"
- var accountPassword = "12345"
- city = "Hyderabad"
-console.log(accountId);
-console.table([accountId, accountEmail, accountPassword, city]); 
+// const accountId = 100;
+// let accountEmail = "abcd@gmail.com"
+// var accountPassword = "12345"
+// city = "Hyderabad"
+// console.log(accountId);
+// console.table([accountId, accountEmail, accountPassword, city]); 
 
 // Datatypes 
 let name = "Abdul";
@@ -27,3 +27,88 @@ let value = true;
 // check for NaN
 
 // ** use typeof operator to get the datatype
+
+// Datatypes - Conversion
+
+//let score = "33"; 
+//let score = "this is maths";
+let score = ""
+console.log(Boolean(score));
+
+// Number(), Boolean(), String(), 
+// "33" => 33
+// "33jkljk" -> NaN
+// "" => false
+// "abcd" => true
+// true => 1
+// false => 0
+
+// OPERATIONS
+/*
+    console.log(2+2);
+    console.log(2*2);
+    console.log(2-2);
+    console.log(2/2);
+    console.log(2%2);
+    console.log(2**2); // power
+*/
+
+// there are many confusing type of variable assignments, operations like 1+ "2" +3 etc, which are not used 
+// in production and is considered as bad practice, just these are used to test js knowledge in interviews sometimes
+// write the code with best readability, resuability and should be optimized
+
+// COMPARISION OF DATATYPES IN JS 
+/* 
+examples:
+1 > 2
+1 < 2
+1 >= 2
+1 <= 2
+1 == 2, other variant checking datatype als0
+1 === 2 Also known as Strict Check
+"2" > 1
+"2ab" > 1
+typical variants in comparision:
+    null > anyNumber
+    always remember 0 is considered false
+    Null, NaN, undefined these behave differently
+    considering engines or any other reason 
+
+*/
+
+// DATATYPES SUMMARY
+/*
+
+Js is a dynamiccally types language, use "typeof" to get the return type of any vairable
+    Primitive datatyes:
+    String
+    number // all floats, everything is number only
+        boolean
+        null
+        undefined
+        symbol
+        bigint
+
+    Reference (non primitive):
+    Array :
+        const a = [1, 2, 3, 4, 5]
+    Objects
+        const obj = { 
+            name: "dem0",
+            age: 5
+        }
+    Functions // functions can be treated same as variables
+        const mutliply = function()  {
+            console.log("this is mutliply function");
+        }
+*/
+
+// example for symbol
+// const id = Symbol("abcd");
+// const anotherId = Symbol("abcd");
+// console.log(id);
+// console.log(anotherId);
+// console.log(id === anotherId) // this gives false
+// this symbol datatype gives uniqueness to any value
+// so better for using it in projects for userid or something 
+
